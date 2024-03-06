8b4ca8e7fbc7e809c8740dd7ca281bcf5629d167
kirandeep singh <kiran0001.gill@gmail.com>
2024-03-06 07:57:27 +0530

    classes objects and class members

---
+++ Classes and Objects 06032024/Program.cs
@@ -0,0 +1,96 @@
+﻿// creating a class 
+
+class Car // it is good practice to start uppercase when naming a class
+{
+    string color = "red"; // variables declared inside class are called fields or attributes
+
+    static void Main(string[] args) { 
+    Car myObj = new Car();
+    Console.WriteLine (myObj.color);
+    }
+}
+
+// multiple objects 
+
+class Truck
+{
+    string color = "Blue";
+    static void Main(string[] args)
+    {
+        Truck myObj = new Truck();
+        Console.WriteLine (myObj.color);
+        Truck myObj2 = new Truck();
+        Console.WriteLine (myObj2.color);
+    }
+}
+
+
+// multiple classes 
+
+class Airplane
+{
+   public string color = "red";
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Airplane myObj = new Airplane();
+
+        Console.WriteLine(myObj.color);
+    }
+}
+
+
+// class members 
+
+// fields and methods inside the class are called members
+
+class MyCar
+{
+    string color= " red";  
+    string name = "safari";
+    public void speed()
+    {
+        Console.WriteLine(" This car runs at 100kmph");
+    }
+
+    static void Main(string[] args)
+    {
+        MyCar myObj = new MyCar();
+        Console.WriteLine(myObj.color);
+        Console.WriteLine(myObj.name);
+        myObj.speed(); 
+        
+        MyCar myObj1 = new MyCar();
+        Console.WriteLine(myObj1.color);
+        Console.WriteLine(myObj1.name);
+        myObj1.speed();
+    }
+}
+
+class MyTruck
+{
+    public string color ;
+    string name ;
+    public void speed()
+    {
+        Console.WriteLine(" This car runs at 100kmph");
+    }
+
+    static void Main(string[] args)
+    {
+        MyTruck myObj = new MyTruck();
+        myObj.color = "red";
+        myObj.name = "safari";
+        Console.WriteLine(myObj.color);
+        Console.WriteLine(myObj.name);
+
+        MyTruck myObj1 = new MyTruck();
+        myObj1.color = "red";
+        myObj1.name = "safari";
+        Console.WriteLine(myObj1.color);
+        Console.WriteLine(myObj1.name);
+    }
+}
