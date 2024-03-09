6ba17fb1eddc458bdcf3b1019b59171ecfeb61ec
kirandeep singh <kiran0001.gill@gmail.com>
2024-03-09 15:04:38 +0530

    inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿class Car
+{
+    public string name = "ford";
+    public void honk()
+    {
+        Console.WriteLine("Zoom Zoom");
+    }
+}
+
+class Mustang : Car
+{
+    public int model = 1997;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car myObj = new Car();
+        Console.WriteLine(myObj.name);
+        myObj.honk();
+    }
+}
+
+
+// if someone wants to keep the class restricted from getting inherited then sealed keyword is used in place of class
