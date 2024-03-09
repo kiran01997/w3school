68eb4633789e77926d2404829df87045bac729a6
kirandeep singh <kiran0001.gill@gmail.com>
2024-03-09 16:35:44 +0530

    abstract and polymorphism

---
+++ polymorphism/Program.cs
@@ -0,0 +1,23 @@
+﻿class Animal
+{
+    public void animalSound()
+    {
+        Console.WriteLine("This is animal sound ");
+    }
+}
+
+class Pig : Animal
+{
+    public void animalSound()
+    {
+        Console.WriteLine(" This is Pig Sound");
+    }
+}
+
+class Dog : Animal
+{
+    public void animalSound()
+    {
+        Console.WriteLine(" This is Dog Sound");
+    }
+}
