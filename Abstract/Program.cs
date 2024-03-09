b15f989f25f4cb1a3a70868a6e0a645cf9d1844d
kirandeep singh <kiran0001.gill@gmail.com>
2024-03-09 17:23:34 +0530

    interface

---
+++ Abstract/Program.cs
@@ -0,0 +1,26 @@
+﻿abstract class Animal
+{
+    public abstract void animalSound();
+    public void sheep()
+    {
+        Console.WriteLine(" The sheep say meeheh meeheh");
+    }
+}
+
+class Dog : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("This is Animal Sound");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog myObj = new Dog();
+        myObj.animalSound();
+        myObj.sheep();
+    }
+}
---
+++ interface/Program.cs
@@ -0,0 +1,59 @@
+﻿interface IAnimal
+{
+    public void animalSound();
+}
+
+class Dog : IAnimal
+{
+    public void animalSound() {
+        Console.WriteLine(" This is Animal Sound ");
+    }
+ }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.animalSound();
+    }
+}
+
+
+//Multiple Interface
+
+interface IFirstInterface
+{
+    void firstInterface();
+}
+
+interface ISecondInterface
+{
+    void secondInterface();
+}
+
+class DemoClass : IFirstInterface, ISecondInterface
+{
+    public void firstInterface() {
+        Console.WriteLine(
+        "this is first interface");
+    }
+
+    public void secondInterface()
+    {
+        Console.WriteLine("this is second ");
+    }
+}
+
+
+
+class Program1
+{
+    static void Main(string[] args)
+    {
+        DemoClass demo = new DemoClass();
+        demo.firstInterface();
+        demo.secondInterface();
+    }
+}
+
